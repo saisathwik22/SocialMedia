2e6d5c3449783f7331298ef3d7eaa43bcd0f6773
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2024-07-13 22:29:16 +0530

    Create auth.controller.js

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const signup = async (req, res) => {
+      res.json({
+            data: "You hit signup endpoint",
+      });
+}
+export const login = async (req, res) => {
+      res.json({
+            data: "You hit login endpoint",
+      });
+}
+export const logout = async (req, res) => {
+      res.json({
+            data: "You hit logout endpoint",
+      });
+}
