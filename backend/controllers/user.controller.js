76aef2dd40de918153cc550e50c50cf4579d8e4d
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-07-14 17:45:17 +0530

    1 - suggested users list fetching

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -37,12 +37,7 @@ export const followUnfollowUser = async (req, res) => {
       await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
       await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
       // send notification to user
-      const newNotification = new Notification({
-        type: "unfollow",
-        from: req.user._id,
-        to: userToModify._id,
-      });
-      await newNotification.save();
+
       res.status(200).json({ message: "User unfollowed successfully" });
     } else {
       // follow the user
@@ -62,6 +57,35 @@ export const followUnfollowUser = async (req, res) => {
     }
   } catch (error) {
     console.log("Error in followUnfollowUser: ", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getSuggestedUsers = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    const usersFollowedByMe = await User.findById(userId).select("following");
+
+    const users = await User.aggregate([
+      {
+        $match: {
+          _id: { $ne: userId },
+        },
+      },
+      { $sample: { size: 10 } },
+    ]);
+
+    const filteredUsers = users.filter(
+      (user) => !usersFollowedByMe.following.includes(user._id)
+    );
+    const suggestedUsers = filteredUsers.slice(0, 4);
+
+    suggestedUsers.forEach((user) => (user.password = null));
+
+    res.status(200).json(suggestedUsers);
+  } catch (error) {
+    console.log("Error in getSuggestedUsers: ", error.message);
     res.status(500).json({ error: error.message });
   }
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,12 +3,13 @@ import { protectRoute } from "../middleware/protectRoute.js";
 import {
   getUserProfile,
   followUnfollowUser,
+  getSuggestedUsers,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
-// router.get("/suggested", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
 // router.post("/update", protectRoute, updateUserProfile);
 
