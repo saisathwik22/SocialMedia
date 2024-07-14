e69a6bd29ff3ebfdfe92c084a7518def40ddd074
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-07-14 18:34:44 +0530

    1 - updateProfile method added for changes in profile details.

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,6 +4,7 @@ import {
   getUserProfile,
   followUnfollowUser,
   getSuggestedUsers,
+  updateUser,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
@@ -11,6 +12,6 @@ const router = express.Router();
 router.get("/profile/:username", protectRoute, getUserProfile);
 router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
-// router.post("/update", protectRoute, updateUserProfile);
+router.post("/update", protectRoute, updateUser);
 
 export default router;
