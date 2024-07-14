676196c3ab6081270a1bf93d08c2c8f2ec36c193
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-07-14 15:21:19 +0530

    1 - Login, logout, Follow, unfollow, notification methods added.

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Notification from "../models/notification.model.js";
 
 export const getUserProfile = async (req, res) => {
   const { username } = req.params;
@@ -21,7 +22,7 @@ export const followUnfollowUser = async (req, res) => {
     const userToModify = await User.findById(id);
     const currentUser = await User.findById(req.user._id);
 
-    if (id === req.user._id.toString()) {
+    if (id === req.user._id) {
       return res
         .status(400)
         .json({ error: "You can't follow/unfollow yourself" });
@@ -36,16 +37,21 @@ export const followUnfollowUser = async (req, res) => {
       await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
       await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
       // send notification to user
-
+      const newNotification = new Notification({
+        type: "unfollow",
+        from: req.user._id,
+        to: userToModify._id,
+      });
+      await newNotification.save();
       res.status(200).json({ message: "User unfollowed successfully" });
     } else {
       // follow the user
       await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
-      await User.findByIdAndUpdate(id, { $push: { following: id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
       // send notification to user
       const newNotification = new Notification({
         type: "follow",
-        from: "req.user._id",
+        from: req.user._id,
         to: userToModify._id,
       });
 
