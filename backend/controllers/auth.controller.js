7f80b9e88d065d70126733f0c95b165c1197ce91
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-07-14 08:41:24 +0530

    1 - Add user model and signup controller in auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,15 +1,70 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-      res.json({
-            data: "You hit signup endpoint",
+  try {
+    const { fullName, username, email, password } = req.body;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email format " });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "Username is already taken " });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email is already taken " });
+    }
+
+    // Hash password - bcryptjs
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        email: newUser.email,
+        followers: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg,
       });
-}
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
 export const login = async (req, res) => {
-      res.json({
-            data: "You hit login endpoint",
-      });
-}
+  res.json({
+    data: "You hit login endpoint",
+  });
+};
+
 export const logout = async (req, res) => {
-      res.json({
-            data: "You hit logout endpoint",
-      });
-}
+  res.json({
+    data: "You hit logout endpoint",
+  });
+};
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,8 @@ dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 5000;
 
-console.log(process.env.MONGO_URI);
+app.use(express.json()); // to parse req.body
+
 
 app.use("/api/auth", authRoutes);
 
