078f1fd136aec4f80bcd2a30fc0554c83a1a7134
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-07-14 11:14:15 +0530

    1 - writing logout controller.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -96,7 +96,11 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-  res.json({
-    data: "You hit logout endpoint",
-  });
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
