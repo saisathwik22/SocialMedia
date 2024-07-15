087e0c95eac104262c82574bfc039bff411a05db
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-07-15 21:57:14 +0530

    1 - createPost, deletePost controllers added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -26,6 +27,7 @@ app.use(cookieParser()); //parse requests to get cookies..
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
