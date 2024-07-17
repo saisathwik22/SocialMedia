4a71dc23b1949b54da8ef7b482413f7f9cd3b70d
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-07-17 13:35:47 +0530

    1 - Notification routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -28,6 +29,7 @@ app.use(cookieParser()); //parse requests to get cookies..
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
