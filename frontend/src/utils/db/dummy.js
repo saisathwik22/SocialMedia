6d9fd0f7b4a4b2136447d8d43bffe04cc9c27ce0
saisathwik22 <reddysaisathwik022@gmail.com>
2024-08-12 14:29:55 +0530

    adding dummy data file. UI

---
+++ frontend/src/utils/db/dummy.js
@@ -0,0 +1,123 @@
+export const POSTS = [
+  {
+    _id: "1",
+    text: "Let's dive into NEXT.JS and build creative applications",
+    img: "/posts/post1.png",
+    user: {
+      username: "sathwik22",
+      profileImg: "/avatars/boy1.png",
+      fullName: "Sai Sathwik",
+    },
+    comments: [
+      {
+        _id: "1",
+        text: "Looks Interesting",
+        user: {
+          username: "janedoe",
+          profileImg: "/avatars/girl1.png",
+          fullName: "Jane Doe",
+        },
+      },
+    ],
+    likes: ["6658s891", "6658s892", "6658s893", "6658s894"],
+  },
+  {
+    _id: "2",
+    text: "How you guys doing?",
+    user: {
+      username: "sathwik22",
+      profileImg: "/avatars/boy2.png",
+      fullName: "Sai Sathwik",
+    },
+    comments: [
+      {
+        _id: "1",
+        text: "Nice Tutorial",
+        user: {
+          username: "janedoe",
+          profileImg: "/avatars/girl2.png",
+          fullName: "Jane Doe",
+        },
+      },
+    ],
+    likes: ["6658s891", "6658s892", "6658s893", "6658s894"],
+  },
+  {
+    _id: "3",
+    text: "Tough choice ahead, what's in demand?",
+    img: "/posts/post2.png",
+    user: {
+      username: "sathwik22",
+      profileImg: "/avatars/boy3.png",
+      fullName: "Sai Sathwik",
+    },
+    comments: [],
+    likes: [
+      "6658s891",
+      "6658s892",
+      "6658s893",
+      "6658s894",
+      "6658s895",
+      "6658s896",
+    ],
+  },
+  {
+    _id: "4",
+    text: "Popular technologies, pick one for a date this weekend!",
+    img: "/posts/post3.png",
+    user: {
+      username: "sathwik22",
+      profileImg: "/avatars/boy3.png",
+      fullName: "Sai Sathwik",
+    },
+    comments: [
+      {
+        _id: "1",
+        text: "Would you mind if I REACT?",
+        user: {
+          username: "janedoe",
+          profileImg: "/avatars/girl3.png",
+          fullName: "Jane Doe",
+        },
+      },
+    ],
+    likes: [
+      "6658s891",
+      "6658s892",
+      "6658s893",
+      "6658s894",
+      "6658s895",
+      "6658s896",
+      "6658s897",
+      "6658s898",
+      "6658s899",
+    ],
+  },
+];
+
+export const USERS_FOR_RIGHT_PANEL = [
+  {
+    _id: "1",
+    fullName: "Sai Sathwik",
+    username: "sathwik22",
+    profileImg: "/avatars/boy2.png",
+  },
+  {
+    _id: "2",
+    fullName: "Jane Doe",
+    username: "janedoe",
+    profileImg: "/avatars/girl1.png",
+  },
+  {
+    _id: "3",
+    fullName: "Hrithik Jain",
+    username: "hrithik19",
+    profileImg: "/avatars/boy3.png",
+  },
+  {
+    _id: "4",
+    fullName: "Tanay Parlikar",
+    username: "tanay29",
+    profileImg: "/avatars/boy1.png",
+  },
+];
