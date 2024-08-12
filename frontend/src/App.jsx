32bdfeab12c2f9673f570949075adcddaa3d3a3e
saisathwik22 <reddysaisathwik022@gmail.com>
2024-08-12 11:22:29 +0530

    Adding Sidebar items UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
+import Sidebar from "./components/common/Sidebar";
 
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
+      <Sidebar />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,5 +1,44 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
-};
+import { useState } from "react";
 
+import Posts from "../../components/common/Posts";
+import CreatePost from "./CreatePost";
+
+const HomePage = () => {
+  const [feedType, setFeedType] = useState("forYou");
+
+  return (
+    <>
+      <div className='flex-[4_4_0] mr-auto border-r border-gray-700 min-h-screen'>
+        {/* HEADER */}
+        <div className='flex w-full border-b border-gray-700'>
+          <div
+            className={
+              "flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
+            }
+            onClick={() => setFeedType("forYou")}
+          >
+            For You
+            {feedType === "forYou" && (
+              <div className='absolute bottom-0 w-10 h-1 rounded-full bg-primary'></div>
+            )}
+          </div>
+          <div
+            className='flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative'
+            onClick={() => setFeedType("following")}
+          >
+            Following
+            {feedType === "following" && (
+              <div className='absolute bottom-0 w-10 h-1 rounded-full bg-primary'></div>
+            )}
+          </div>
+        </div>
+        {/* Create POST Input */}
+        <CreatePost />
+
+        {/* POSTS */}
+        <Posts />
+      </div>
+    </>
+  );
+};
 export default HomePage;
