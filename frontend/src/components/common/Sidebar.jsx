0418a8b68a6808cbbf562772142f2d85a36c273c
saisathwik22 <reddysaisathwik022@gmail.com>
2024-08-13 14:22:01 +0530

    removed hard coded data from sidebar, fetching from database.

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -5,7 +5,7 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { toast } from "react-hot-toast";
 
@@ -34,12 +34,7 @@ const Sidebar = () => {
       toast.error("Logout failed");
     },
   });
-  const data = {
-    fullName: "Sai Sathwik",
-    username: "sathwik22",
-    profileImg: "/avatars/boy1.png",
-  };
-
+  const { data: authUser } = useQuery({ queryKey: ["authUser"] });
   return (
     <div className='md:flex-[2_2_0] w-18 max-w-52'>
       <div className='sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-20 md:w-full'>
@@ -67,7 +62,7 @@ const Sidebar = () => {
           </li>
           <li className='flex justify-center md:justify-start'>
             <Link
-              to={`/profile/${data?.username}`}
+              to={`/profile/${authUser?.username}`}
               className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
             >
               <FaUser className='w-6 h-6' />
@@ -75,22 +70,22 @@ const Sidebar = () => {
             </Link>
           </li>
         </ul>
-        {data && (
+        {authUser && (
           <Link
-            to={`/profile/${data.username}`}
+            to={`/profile/${authUser.username}`}
             className='mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full'
           >
             <div className='avatar hidden md:inline-flex'>
               <div className='w-8 rounded-full'>
-                <img src={data?.profileImg || "/avatar-placeholder.png"} />
+                <img src={authUser?.profileImg || "/avatar-placeholder.png"} />
               </div>
             </div>
             <div className='flex justify-between flex-1'>
               <div className='hidden md:block'>
                 <p className='text-white font-bold text-sm w-20 truncate'>
-                  {data?.fullName}
+                  {authUser?.fullName}
                 </p>
-                <p className='text-slate-500 text-sm'>@{data?.username}</p>
+                <p className='text-slate-500 text-sm'>@{authUser?.username}</p>
               </div>
               <BiLogOut
                 className='w-5 h-5 cursor-pointer'
