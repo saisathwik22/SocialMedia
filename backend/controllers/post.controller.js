4547fdfe383cf65fbbe71bb3ed9674650feeaf16
saisathwik22 <reddysaisathwik022@gmail.com>
2024-08-15 20:05:32 +0530

    like/unlike the post feature added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -107,7 +107,11 @@ export const likeUnlikePost = async (req, res) => {
       // Unlike the post
       await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
       await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
-      res.status(200).json({ message: "Post unliked successfully" });
+
+      const updateLikes = post.likes.filter(
+        (id) => id.toString() !== userId.toString()
+      );
+      res.status(200).json(updateLikes);
     } else {
       // Like post
       post.likes.push(userId);
@@ -121,7 +125,8 @@ export const likeUnlikePost = async (req, res) => {
       });
       await notification.save();
 
-      res.status(200).json({ message: "Post liked successfully" });
+      const updatedLikes = post.likes;
+      res.status(200).json(updatedLikes);
     }
   } catch (error) {
     console.log("Error in likeUnlikePost controller: ", error);
