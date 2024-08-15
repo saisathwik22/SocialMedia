074048c4001970e85873daf6e815c675706bd633
saisathwik22 <reddysaisathwik022@gmail.com>
2024-08-15 13:05:01 +0530

    payload limit updted to 5mb (prevent Dos attack)

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); // to parse req.body
+app.use(express.json({ limit: "5mb" })); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use(cookieParser()); //parse requests to get cookies..
