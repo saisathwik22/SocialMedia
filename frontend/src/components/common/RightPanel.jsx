6e89ffff1d981d0c0e7c6e1cdefa66842648529e
saisathwik22 <reddysaisathwik022@gmail.com>
2024-08-15 13:55:40 +0530

    suggested users feature added.

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,9 +1,26 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
+
+import { useQuery } from "@tanstack/react-query";
 
 const RightPanel = () => {
-  const isLoading = false;
+  const { data: suggestedUsers, isLoading } = useQuery({
+    queryKey: ["suggestedUsers"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/users/suggested");
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.message || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+  });
+
+  if (suggestedUsers.length === 0) return <div className='md:w-64 w-0'></div>;
 
   return (
     <div className='hidden lg:block my-4 mx-2'>
@@ -20,24 +37,26 @@ const RightPanel = () => {
             </>
           )}
           {!isLoading &&
-            USERS_FOR_RIGHT_PANEL?.map((user) => (
+            suggestedUsers?.map((user) => (
               <Link
-                to={`/profile/${user.username}`}
+                to={`/profile/${user?.username}`}
                 className='flex items-center justify-between gap-4'
                 key={user._id}
               >
                 <div className='flex gap-2 items-center'>
                   <div className='avatar'>
                     <div className='w-8 rounded-full'>
-                      <img src={user.profileImg || "/avatar-placeholder.png"} />
+                      <img
+                        src={user?.profileImg || "/avatar-placeholder.png"}
+                      />
                     </div>
                   </div>
                   <div className='flex flex-col'>
                     <span className='font-semibold tracking-tight truncate w-28'>
-                      {user.fullName}
+                      {user?.fullName}
                     </span>
                     <span className='text-sm text-slate-500'>
-                      @{user.username}
+                      @{user?.username}
                     </span>
                   </div>
                 </div>
