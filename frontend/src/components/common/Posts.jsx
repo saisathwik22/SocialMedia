cd5d027a0c102e27f15c1d0e66e2c6adb6abd89b
saisathwik22 <reddysaisathwik022@gmail.com>
2024-08-16 08:46:33 +0530

    Profie page fetching added.

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -4,13 +4,17 @@ import PostSkeleton from "../skeletons/PostSkeleton";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
-const Posts = ({ feedType }) => {
+const Posts = ({ feedType, username, userId }) => {
   const getPostEndpoint = () => {
     switch (feedType) {
       case "forYou":
         return "/api/posts/all";
       case "following":
         return "/api/posts/following";
+      case "posts":
+        return `/api/posts/user/${username}`;
+      case "likes":
+        return `/api/posts/likes/${userId}`;
       default:
         return "/api/posts/all";
     }
@@ -43,7 +47,7 @@ const Posts = ({ feedType }) => {
 
   useEffect(() => {
     refetch();
-  }, [feedType, refetch]);
+  }, [feedType, refetch, username]);
 
   return (
     <>
--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -1,5 +1,5 @@
-import { useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 import Posts from "../../components/common/Posts";
 import ProfileHeaderSkeleton from "../../components/skeletons/ProfileHeaderSkeleton";
@@ -11,6 +11,8 @@ import { FaArrowLeft } from "react-icons/fa6";
 import { IoCalendarOutline } from "react-icons/io5";
 import { FaLink } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
+import { useQuery } from "@tanstack/react-query";
+import { formatMemberSinceDate } from "../../utils/date";
 // import { useQuery } from "@tanstack/react-query";
 
 const ProfilePage = () => {
@@ -21,20 +23,30 @@ const ProfilePage = () => {
   const coverImgRef = useRef(null);
   const profileImgRef = useRef(null);
 
-  const isLoading = false;
+  const { username } = useParams();
+
   const isMyProfile = true;
 
-  const user = {
-    _id: "1",
-    fullName: "Sai Sathwik",
-    username: "sathwik22",
-    profileImg: "/avatars/boy2.png",
-    coverImg: "/cover.png",
-    bio: "Software Enthusiast, MERN developer.",
-    link: "https://github.com/saisathwik22",
-    following: ["1", "2", "3"],
-    followers: ["1", "2", "3"],
-  };
+  const {
+    data: user,
+    isLoading,
+    refetch,
+    isRefetching,
+  } = useQuery({
+    queryKey: ["userProfile"],
+    queryFn: async () => {
+      try {
+        const res = await fetch(`/api/users/profile/${username}`);
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
 
   const handleImgChange = (e, state) => {
     const file = e.target.files[0];
@@ -48,16 +60,22 @@ const ProfilePage = () => {
     }
   };
 
+  useEffect(() => {
+    refetch();
+  }, [username, refetch]);
+
+  const memberSinceDate = formatMemberSinceDate(user?.createdAt);
+
   return (
     <>
       <div className='flex-[4_4_0]  border-r border-gray-700 min-h-screen '>
         {/* HEADER */}
-        {isLoading && <ProfileHeaderSkeleton />}
-        {!isLoading && !user && (
+        {(isLoading || isRefetching) && <ProfileHeaderSkeleton />}
+        {!isLoading && !isRefetching && !user && (
           <p className='text-center text-lg mt-4'>User not found</p>
         )}
         <div className='flex flex-col'>
-          {!isLoading && user && (
+          {!isLoading && !isRefetching && user && (
             <>
               <div className='flex gap-10 px-4 py-2 items-center'>
                 <Link to='/'>
@@ -167,9 +185,9 @@ const ProfilePage = () => {
                     </div>
                   )}
                   <div className='flex gap-2 items-center'>
-                    <IoCalendarOutline className='w-4 h-4 text-slate-500' />
-                    <span className='text-sm text-slate-500'>
-                      Joined July 2021
+                    <IoCalendarOutline className='w-4 h-4 text-slate-400' />
+                    <span className='text-sm text-slate-400'>
+                      {memberSinceDate}
                     </span>
                   </div>
                 </div>
@@ -178,13 +196,13 @@ const ProfilePage = () => {
                     <span className='font-bold text-xs'>
                       {user?.following.length}
                     </span>
-                    <span className='text-slate-500 text-xs'>Following</span>
+                    <span className='text-slate-400 text-xs'>Following</span>
                   </div>
                   <div className='flex gap-1 items-center'>
                     <span className='font-bold text-xs'>
                       {user?.followers.length}
                     </span>
-                    <span className='text-slate-500 text-xs'>Followers</span>
+                    <span className='text-slate-400 text-xs'>Followers</span>
                   </div>
                 </div>
               </div>
@@ -211,7 +229,7 @@ const ProfilePage = () => {
             </>
           )}
 
-          <Posts />
+          <Posts feedType={feedType} username={username} userId={user?._id} />
         </div>
       </div>
     </>
